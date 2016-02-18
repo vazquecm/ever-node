c6785db962b69f9e016b1280b915244f80e5fde9
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-18 15:08:14 -0600

    refactored controller file to use just one object literal

--- controllers/note.js
+++ controllers/note.js
@@ -2,57 +2,58 @@
 
 const Note = require('../models/note');
 
-module.exports.edit = (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
+module.exports = {
+  edit (req, res) {
+    Note.findById(req.params.id, (err, note) => {
+      if (err) throw err;
 
-    res.render('new-note', {note: note});
-  });
-};
+      res.render('new-note', {note: note});
+    });
+  },
 
-module.exports.update = (req, res) => {
-  Note.findByIdAndUpdate(req.params.id,
-    req.body, (err, note) => {
+  update (req, res) {
+    Note.findByIdAndUpdate(req.params.id,
+      req.body, (err, note) => {
+        if (err) throw err;
+
+        res.redirect(`/notes/${note._id}`);
+      }
+    );
+  },
+
+  index (req, res) {
+    Note.find({}, (err, notes) => {
+      if (err) throw err;
+
+      res.render('notes-index', {notes: notes});
+    });
+  },
+
+  newNote (req, res) {
+    res.render('new-note');
+  },
+
+  show (req, res) {
+    Note.findById(req.params.id, (err, note) => {
+      if (err) throw err;
+
+      res.render('show-note', {note: note});
+    });
+  },
+
+  create (req, res) {
+    Note.create(req.body, (err, note) => {
       if (err) throw err;
 
       res.redirect(`/notes/${note._id}`);
-    }
-  );
-};
+    });
+  },
 
+  destroy (req, res) {
+    Note.findByIdAndRemove(req.params.id, (err) => {
+      if (err) throw err;
 
-module.exports.index = (req, res) => {
-  Note.find({}, (err, notes) => {
-    if (err) throw err;
-
-    res.render('notes-index', {notes: notes});
-  });
-};
-
-module.exports.newNote = (req, res) => {
-  res.render('new-note');
-};
-
-module.exports.show = (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
-    res.render('show-note', {note: note});
-  });
-};
-
-module.exports.create = (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-
-    res.redirect(`/notes/${note._id}`);
-  });
-};
-
-module.exports.destroy = (req, res) => {
-  Note.findByIdAndRemove(req.params.id, (err) => {
-    if (err) throw err;
-
-    res.redirect('/notes');
-  });
+      res.redirect('/notes');
+    });
+  }
 };
