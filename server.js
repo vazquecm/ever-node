549666b8eedcc63c47fb3185cc8d023dab6473cb
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-18 11:25:01 -0600

    show view created

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const Note = mongoose.model('Notes', mongoose.Schema({
+  title: String,
+  text: String
+}));
 
 app.set('view engine', 'jade');
 
@@ -14,17 +20,33 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.get('/', (req, res)  => {
   res.send('Server Running');
 });
+
 // making a new note in app
 app.get('/notes/new', (req, res) => {
   res.render('new-note');
 });
 
-app.post('/notes', (req, res) => {
-  console.log(req.body);
-  // specifies the path being used
-  res.redirect('/');
+// viewing a note
+app.get('/notes/:id', (req, res) => {
+  Note.findById(req.params.id, (err, note) => {
+  if (err) throw err;
+  res.render('show-note', {note: note});
+ });
 });
+
+app.post('/notes', (req, res) => {
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+  // specifies the path being used
+  res.redirect(`/notes/${note._id}`);
+  });
+});
+
+mongoose.connect('mongodb://localhost:27017/ever-node', (err) => {
+  if (err) throw err;
+
 
 app.listen(port, () => {
   console.log(`Evernode server running on port: ${port}`);
-})
+ });
+});
